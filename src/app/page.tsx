b23ff8fd17617301dd89b2824547c253c6be3dfc
landarisafa <landarisafa@gmail.com>
2023-11-09 10:18:22 +0100

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import { Button, ConfigProvider } from 'antd';
+import theme from '../theme/themeConfig';
+import { Carousel } from 'antd';
+
+const contentStyle: React.CSSProperties = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
 export default function Home() {
+  const onChange = (currentSlide: number) => {
+    console.log(currentSlide);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <ConfigProvider theme={theme}>
+      {/* <div className="App">
+        <Button type="primary">Button</Button> */}
+
+      <Carousel afterChange={onChange}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h3 style={{
+            margin: 0,
+            height: '160px',
+            color: '#fff',
+            lineHeight: '160px',
+            textAlign: 'center',
+            background: '#364d79',
+          }}>1</h3>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div>
+          <h3 style={{
+            margin: 0,
+            height: '160px',
+            color: '#fff',
+            lineHeight: '160px',
+            textAlign: 'center',
+            background: '#364d79',
+          }}>2</h3>
+        </div>
+        <div>
+          <h3 style={{
+            margin: 0,
+            height: '160px',
+            color: '#fff',
+            lineHeight: '160px',
+            textAlign: 'center',
+            background: '#364d79',
+          }}>3</h3>
+        </div>
+        <div>
+          <h3 style={{
+            margin: 0,
+            height: '160px',
+            color: '#fff',
+            lineHeight: '160px',
+            textAlign: 'center',
+            background: '#364d79',
+          }}>4</h3>
+        </div>
+      </Carousel>
+      {/* </div> */}
+    </ConfigProvider>
   )
 }
